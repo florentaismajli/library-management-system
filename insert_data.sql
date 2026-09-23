0a72f8213dbb7dabe0c0823adf20aac1e184ad1c
florentaismajli <florentaismajli36@gmail.com>
2026-09-23 11:10:16 +0200

    Create insert_data.sql

---
+++ insert_data.sql
@@ -0,0 +1,46 @@
+INSERT INTO books
+(title, author, genre, publication_year, price, stock, rating, language)
+VALUES
+('ABC-ja ime', 'Adelina Mamaqi', 'Arsim', 2021, 8.50, 10, 4.8, 'Shqip'),
+
+('Tregime për fëmijë', 'Bedri Dedja', 'Tregime', 2019, 12.00, 7, 4.7, 'Shqip'),
+
+('Përralla shqiptare', 'Agim Deva', 'Përralla', 2022, 10.50, 8, NULL, 'Shqip'),
+
+('Vjersha për fëmijë', 'Vehbi Kikaj', 'Poezi', 2018, 9.50, 5, 4.6, 'Shqip'),
+
+('Lulet e Verës', 'Naim Frashëri', 'Poezi', 2020, 14.00, 6, 4.9, 'Shqip'),
+
+('Bagëti e Bujqësi', 'Naim Frashëri', 'Poezi', 2017, 16.50, 0, 4.8, 'Shqip'),
+
+('Fëmijët dhe natyra', 'Adelina Mamaqi', 'Arsim', 2023, 11.00, 9, NULL, 'Shqip'),
+
+('Aventurat e vogla', 'Agim Deva', 'Aventurë', 2021, 18.00, 4, 4.4, 'Shqip'),
+
+('Yjet e vegjël', 'Xhevahir Spahiu', 'Poezi', 2016, 13.50, 3, 4.2, 'Shqip'),
+
+('Shoku im i klasës', 'Bedri Dedja', 'Tregime', 2015, 15.00, 2, 4.1, 'Shqip'),
+
+('Kopshti magjik', 'Agim Deva', 'Fantazi', 2024, 21.00, 6, 4.7, 'Shqip'),
+
+('Udhëtimi nëpër libra', 'Adelina Mamaqi', 'Aventurë', 2022, 19.50, 0, NULL, 'Shqip'),
+
+('Përralla nga vendlindja', 'Vehbi Kikaj', 'Përralla', 2014, 10.00, 5, 4.0, 'Shqip'),
+
+('Zëri i fëmijëve', 'Xhevahir Spahiu', 'Poezi', 2019, 17.50, 8, 4.3, 'Shqip'),
+
+('Miqtë e pyllit', 'Agim Deva', 'Fantazi', 2025, 24.00, 4, 4.9, 'Shqip'),
+
+('Ditari i klasës', 'Bedri Dedja', 'Tregime', 2021, 13.00, 10, NULL, 'Shqip'),
+
+('Qerbelaja', 'Naim Frashëri', 'Histori', 1985, 20.00, 0, 4.5, 'Shqip'),
+
+('Tregime për natyrën', 'Adelina Mamaqi', 'Arsim', 2008, 7.50, 3, 3.9, 'Shqip'),
+
+('Lexime të bukura', 'Agim Deva', 'Arsim', 2023, 28.00, 7, 4.6, 'Shqip'),
+
+('Bota e fëmijëve', 'Vehbi Kikaj', 'Tregime', 1998, 18.50, 1, 4.1, 'Shqip'),
+
+('My Albanian Reading Book', 'Adelina Mamaqi', 'Arsim', 2022, 12.50, 5, 4.4, 'English'),
+
+('Little Albanian Stories', 'Agim Deva', 'Tregime', 2024, 15.50, 2, NULL, 'English');
